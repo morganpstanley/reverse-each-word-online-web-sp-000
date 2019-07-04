f56ac2cf5927411d6249353376d389132a433d76
Morgan Stanley <morganpstanley@gmail.com>
2019-07-04 02:33:34 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,6 @@
 def reverse_each_word(sentence)
   sentence_array = sentence.split(" ")
-  setence_array = sentence_array.reverse
-  puts sentence_array
+  puts sentence_array.reverse
 end
 
 sentence = "Hello, how are you"
