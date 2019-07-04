377a7d06ec59c8a9c1cd5bb284c46364226d9b1c
Morgan Stanley <morganpstanley@gmail.com>
2019-07-04 02:40:08 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
   sentence_array = sentence.split(" ")
   sentence_array.map do |word|
-    word.reverse
+    sentence2 << word.split("").reverse
   puts sentence_array
   end
 end
